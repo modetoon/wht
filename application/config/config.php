6a881451daef80ed8c4a21e2e405f1168fe8e969
modetoon <natthaphol@gmail.com>
2016-07-30 14:42:07 +0700

    Test commit
    Test commit

--- application/config/config.php
+++ application/config/config.php
@@ -22,7 +22,7 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/wht/';
+$config['base_url'] = 'http://localhost:81/=skywave/wht/';
 
 /*
 |--------------------------------------------------------------------------
